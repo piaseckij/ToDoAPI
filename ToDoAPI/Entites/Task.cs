b342915cc31cfe84d5cce1ecd1af8dc78fc626c0
Jakub Piasecki <jakkubpiasecki87@gmail.com>
2022-01-21 13:24:26 +0100

    Add entity framework; first migration

---
+++ ToDoAPI/Entites/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ToDoAPI.Entites
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+
+        //public int UserId { get; set; }
+        //public virtual User User{ get; set; }
+
+
+    }
+}
---
+++ ToDoAPI/Entites/TaskDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace ToDoAPI.Entites
+{
+    public class TaskDbContext:DbContext
+    {
+        private string _connectionstring =
+            "Server=localhost;Database=TasksDb;Trusted_Connection=True;";
+
+        //public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            modelBuilder.Entity<Task>()
+                .Property(t => t.IsDone)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionstring);
+        }
+    }
+}
