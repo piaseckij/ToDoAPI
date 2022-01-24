abe8c3a674c54313468fc8f4f842ed335bef69b0
Jakub Piasecki <jakkubpiasecki87@gmail.com>
2022-01-24 14:14:07 +0100

    Add JWT Authentication config

--- ToDoAPI/Startup.cs
+++ ToDoAPI/Startup.cs
@@ -9,10 +9,12 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using ToDoAPI.Entites;
 using ToDoAPI.Middleware;
 using ToDoAPI.Models;
@@ -33,6 +35,27 @@ namespace ToDoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var authenticationSettings = new AuthenticationSettings();
+            
+            Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = "Bearer";
+                option.DefaultScheme = "Bearer";
+                option.DefaultChallengeScheme = "Bearer";
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = authenticationSettings.JwtIssuer,
+                    ValidAudience = authenticationSettings.JwtIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+                };
+            });
+
             services.AddControllers().AddFluentValidation();
             services.AddDbContext<ToDoDbContext>();
             services.AddScoped<ToDoSeeder>();
@@ -55,6 +78,7 @@ namespace ToDoAPI
             }
 
             app.UseMiddleware<ErrorHandlingMiddleware>();
+            app.UseAuthentication();
             app.UseHttpsRedirection();
 
             app.UseRouting();
