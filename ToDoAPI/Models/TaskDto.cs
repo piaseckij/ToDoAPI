aa8994626fd53586896e1dfe80487df8430c69b5
Jakub Piasecki <jakkubpiasecki87@gmail.com>
2022-01-27 15:38:10 +0100

    Add required and maxLenght to TaskDto Name

--- ToDoAPI/Models/TaskDto.cs
+++ ToDoAPI/Models/TaskDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace ToDoAPI.Models
     public class TaskDto
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
         public string Description { get; set; }
         public bool IsDone { get; set; }
